e1a807502cdc0bc4e3349da5b422cd34e8962df9
Ronildo Souza <ronildo_rns@hotmail.com>
2017-05-17 15:35:07 -0300

    .

--- LivrosQueJaLi/LivrosQueJaLi/ViewModels/BaseViewModel.cs
+++ LivrosQueJaLi/LivrosQueJaLi/ViewModels/BaseViewModel.cs
@@ -66,7 +66,7 @@ namespace LivrosQueJaLi.ViewModels
             {
                 IsBusy = true;
 
-                await action();
+                await action().ConfigureAwait(false);
 
                 IsBusy = false;
             }
